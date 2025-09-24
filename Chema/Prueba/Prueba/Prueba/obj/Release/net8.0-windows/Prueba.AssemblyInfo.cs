d4cb2252d45d90c56b205bcece58a434c8f40018
Mario Núñez García <mnunegarcia24@gmail.com>
2025-09-24 14:04:37 +0200

    Prueba
    prueba

--- Chema/Prueba/Prueba/Prueba/obj/Release/net8.0-windows/Prueba.AssemblyInfo.cs
+++ Chema/Prueba/Prueba/Prueba/obj/Release/net8.0-windows/Prueba.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2457949e65e204fa52fb83615d3e5d32c2a948")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba")]
