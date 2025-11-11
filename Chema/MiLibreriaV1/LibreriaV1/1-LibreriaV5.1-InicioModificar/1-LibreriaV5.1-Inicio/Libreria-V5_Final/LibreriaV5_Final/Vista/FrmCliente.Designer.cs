05a5d09b380d16a8861da48b4f16199db317078c
Mario Núñez García <mnunegarcia24@gmail.com>
2025-11-11 14:14:08 +0100

    FormClientes

---
+++ Chema/MiLibreriaV1/LibreriaV1/1-LibreriaV5.1-InicioModificar/1-LibreriaV5.1-Inicio/Libreria-V5_Final/LibreriaV5_Final/Vista/FrmCliente.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace LibreriaV5_Final.Vista
+{
+    partial class FrmCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textDireccion = new System.Windows.Forms.TextBox();
+            this.textTelefono = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textApellidos = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listClientes = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.buttonModificar = new System.Windows.Forms.Button();
+            this.buttonNuevo = new System.Windows.Forms.Button();
+            this.buttonBaja = new System.Windows.Forms.Button();
+            this.buttonAlta = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 42F);
+            this.label1.Location = new System.Drawing.Point(127, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 79);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cliente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(91, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(77, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Telefono";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(104, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(72, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Apellidos";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(79, 344);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 25);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Direccion";
+            // 
+            // textNombre
+            // 
+            this.textNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textNombre.Location = new System.Drawing.Point(187, 167);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(202, 30);
+            this.textNombre.TabIndex = 8;
+            this.textNombre.TextChanged += new System.EventHandler(this.textNombre_TextChanged);
+            // 
+            // textDireccion
+            // 
+            this.textDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textDireccion.Location = new System.Drawing.Point(187, 348);
+            this.textDireccion.Name = "textDireccion";
+            this.textDireccion.Size = new System.Drawing.Size(202, 30);
+            this.textDireccion.TabIndex = 9;
+            // 
+            // textTelefono
+            // 
+            this.textTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textTelefono.Location = new System.Drawing.Point(187, 260);
+            this.textTelefono.Name = "textTelefono";
+            this.textTelefono.Size = new System.Drawing.Size(202, 30);
+            this.textTelefono.TabIndex = 10;
+            // 
+            // textEmail
+            // 
+            this.textEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textEmail.Location = new System.Drawing.Point(187, 305);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(202, 30);
+            this.textEmail.TabIndex = 11;
+            // 
+            // textApellidos
+            // 
+            this.textApellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textApellidos.Location = new System.Drawing.Point(187, 215);
+            this.textApellidos.Name = "textApellidos";
+            this.textApellidos.Size = new System.Drawing.Size(202, 30);
+            this.textApellidos.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.Location = new System.Drawing.Point(430, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Mensajes:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(539, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 30);
+            this.textBox1.TabIndex = 14;
+            // 
+            // listClientes
+            // 
+            this.listClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.listClientes.FormattingEnabled = true;
+            this.listClientes.ItemHeight = 18;
+            this.listClientes.Location = new System.Drawing.Point(537, 150);
+            this.listClientes.Name = "listClientes";
+            this.listClientes.Size = new System.Drawing.Size(267, 400);
+            this.listClientes.TabIndex = 15;
+            this.listClientes.SelectedIndexChanged += new System.EventHandler(this.listClientes_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.buttonSalir);
+            this.panel1.Controls.Add(this.buttonModificar);
+            this.panel1.Controls.Add(this.buttonNuevo);
+            this.panel1.Controls.Add(this.buttonBaja);
+            this.panel1.Controls.Add(this.buttonAlta);
+            this.panel1.Location = new System.Drawing.Point(167, 398);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(253, 191);
+            this.panel1.TabIndex = 16;
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonSalir.Location = new System.Drawing.Point(13, 133);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(222, 40);
+            this.buttonSalir.TabIndex = 4;
+            this.buttonSalir.Text = "Salir";
+            this.buttonSalir.UseVisualStyleBackColor = true;
+            this.buttonSalir.Click += new System.EventHandler(this.buttonSalir_Click);
+            // 
+            // buttonModificar
+            // 
+            this.buttonModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonModificar.Location = new System.Drawing.Point(135, 78);
+            this.buttonModificar.Name = "buttonModificar";
+            this.buttonModificar.Size = new System.Drawing.Size(100, 40);
+            this.buttonModificar.TabIndex = 3;
+            this.buttonModificar.Text = "Modificar";
+            this.buttonModificar.UseVisualStyleBackColor = true;
+            this.buttonModificar.Click += new System.EventHandler(this.buttonModificar_Click);
+            // 
+            // buttonNuevo
+            // 
+            this.buttonNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonNuevo.Location = new System.Drawing.Point(13, 78);
+            this.buttonNuevo.Name = "buttonNuevo";
+            this.buttonNuevo.Size = new System.Drawing.Size(100, 40);
+            this.buttonNuevo.TabIndex = 2;
+            this.buttonNuevo.Text = "Nuevo";
+            this.buttonNuevo.UseVisualStyleBackColor = true;
+            this.buttonNuevo.Click += new System.EventHandler(this.buttonNuevo_Click);
+            // 
+            // buttonBaja
+            // 
+            this.buttonBaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonBaja.Location = new System.Drawing.Point(135, 22);
+            this.buttonBaja.Name = "buttonBaja";
+            this.buttonBaja.Size = new System.Drawing.Size(100, 40);
+            this.buttonBaja.TabIndex = 1;
+            this.buttonBaja.Text = "Baja";
+            this.buttonBaja.UseVisualStyleBackColor = true;
+            this.buttonBaja.Click += new System.EventHandler(this.buttonBaja_Click);
+            // 
+            // buttonAlta
+            // 
+            this.buttonAlta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonAlta.Location = new System.Drawing.Point(13, 22);
+            this.buttonAlta.Name = "buttonAlta";
+            this.buttonAlta.Size = new System.Drawing.Size(100, 40);
+            this.buttonAlta.TabIndex = 0;
+            this.buttonAlta.Text = "Alta";
+            this.buttonAlta.UseVisualStyleBackColor = true;
+            this.buttonAlta.Click += new System.EventHandler(this.buttonAlta_Click);
+            // 
+            // FrmCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 650);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listClientes);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textApellidos);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.textTelefono);
+            this.Controls.Add(this.textDireccion);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCliente";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmCliente_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textDireccion;
+        private System.Windows.Forms.TextBox textTelefono;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textApellidos;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listClientes;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonBaja;
+        private System.Windows.Forms.Button buttonAlta;
+        private System.Windows.Forms.Button buttonSalir;
+        private System.Windows.Forms.Button buttonModificar;
+        private System.Windows.Forms.Button buttonNuevo;
+    }
+}
---
+++ Chema/MiLibreriaV1/LibreriaV1/1-LibreriaV5.1-InicioModificar/1-LibreriaV5.1-Inicio/Libreria-V5_Final/LibreriaV5_Final/Vista/FrmCliente.cs
@@ -0,0 +1,164 @@
+﻿using LibreriaV5_Final.Modelo;
+using LibreriaV5_Final.Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibreriaV5_Final.Vista
+{
+    public partial class FrmCliente : Form
+    {
+        public FrmCliente()
+        {
+            InitializeComponent();
+        }
+        private void buttonNuevo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAlta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBaja_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonModificar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSalir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listClientes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // METODOS PRIVADOS
+        private TCliente RecogerDatosPantalla()
+        {
+            TCliente cliente = null;
+            string codCliente, nombre, apellidos, email, telefono, direccion;
+            nombre = textNombre.Text;
+            apellidos = textApellidos.Text;
+            email = textEmail.Text;
+            telefono = textTelefono.Text;
+            direccion = textDireccion.Text;
+
+            if (nombre.Count() != 0 && apellidos.Count() != 0 && email.Count() != 0 && telefono.Count() != 0 && direccion.Count() != 0)
+            {
+                if (((TCliente)listClientes.SelectedItem) == null)
+                    cliente = new TCliente(nombre, apellidos, email, telefono, direccion);
+                else
+                {
+                    codCliente = ((TLibro)lstLibros.SelectedItem).CodLibro;
+                    libro = new TLibro(codCliente, apellidos, nombre, tema, email, telefono, formatoUno, formatoDos, formatoTres, estado);
+                }
+            }
+            return libro;
+        }
+
+        private void ObtenerTodosLibros()
+        {
+            try
+            {
+                List<object> libros = new List<object>();
+                foreach (TLibro item in control.Obtener(new TLibro().GetType()))
+                {
+                    if (item.Borrado.Equals("0"))
+                    {
+                        lstLibros.Items.Add(item);
+                    }
+                }
+                lstLibros.ClearSelected();
+                VaciarPantalla();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ObtenerTemas()
+        {
+            try
+            {
+                foreach (TTema item in control.Obtener(new TTema().GetType()))
+                {
+                    cbxTemas.Items.Add(item.Tema);
+                }
+                cbxTemas.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void VaciarPantalla()
+        {
+            txtAutor.Text = "";
+            //txtMensaje.Text = "";
+            txtPaginas.Text = "";
+            txtPrecio.Text = "";
+            txtTitulo.Text = "";
+            rbReedicion.Checked = false;
+            chkCartone.Checked = false;
+            chkRustica.Checked = false;
+            chkTapaDura.Checked = false;
+            cbxTemas.SelectedIndex = 0;
+        }
+
+        private void RellenarCampos(TLibro sender)
+        {
+            textNombre.Text = sender.Autor;
+            textApellidos.Text = sender.apellidos;
+            textDni.Text = sender.Dni;
+            textEmail.Text = sender.Titulo;
+            textDireccion.Text = sender.Direccion;
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void textNombre_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void FrmCliente_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
